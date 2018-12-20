e2a9fa8a1456e794c43c2790c3765f23ef299953
TheSimonLam <thesimonlam@gmail.com>
2018-12-20 22:05:40 +0000

    yes

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Start/>
+  </div>
+</template>
+
+<script>
+import Start from './components/Start.vue'
+
+export default {
+  name: 'app',
+  components: {
+      Start
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Start.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <select v-model="selectedPlayer">
+      <option value="" disabled hidden>Select Player</option>
+      <option v-for="player in players">{{player.name}}</option>
+    </select>
+
+  </div>
+</template>
+
+<script>
+const fs = require('fs');
+
+export default {
+  name: 'Start',
+  data(){
+      return{
+
+      }
+  },
+  methods:{
+
+  },
+  created(){
+    //TODO: Get the things somehow
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
